30fc4f9767811afb1e65ffffb6b049b6990399f9
Paiva <85632116+Paibass@users.noreply.github.com>
2025-11-11 21:53:23 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Archivo as V0_Font_Archivo, Inconsolata as V0_Font_Inconsolata, Source_
 const _archivo = V0_Font_Archivo({ subsets: ['latin'], weight: ["100","200","300","400","500","600","700","800","900"], variable: '--v0-font-archivo' })
 const _inconsolata = V0_Font_Inconsolata({ subsets: ['latin'], weight: ["200","300","400","500","600","700","800","900"], variable: '--v0-font-inconsolata' })
 const _sourceSerif_4 = V0_Font_Source_Serif_4({ subsets: ['latin'], weight: ["200","300","400","500","600","700","800","900"], variable: '--v0-font-source-serif-4' })
-const _v0_fontVariables = `${_archivo.variable} ${_inconsolata.variable} ${_sourceSerif_4.variable}`
+const _v0_fontVariables = `${_archivo.variable}`
 
 export const metadata: Metadata = {
   title: "KFC Planificador de Turnos",
