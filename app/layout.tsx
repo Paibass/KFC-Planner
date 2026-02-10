7f6041e0128123db064e49c33ffd091d661cdb31
Paiva <85632116+Paibass@users.noreply.github.com>
2026-02-10 10:09:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
-import { fontVariables } from "@/lib/fonts"
-import { Geist as V0_Font_Geist, Geist_Mono as V0_Font_Geist_Mono, Source_Serif_4 as V0_Font_Source_Serif_4 } from 'next/font/google'
+
+import { Archivo as V0_Font_Archivo, Inconsolata as V0_Font_Inconsolata, Source_Serif_4 as V0_Font_Source_Serif_4 } from 'next/font/google'
 
 // Initialize fonts
-const _geist = V0_Font_Geist({ subsets: ['latin'], weight: ["100","200","300","400","500","600","700","800","900"], variable: '--v0-font-geist' })
-const _geistMono = V0_Font_Geist_Mono({ subsets: ['latin'], weight: ["100","200","300","400","500","600","700","800","900"], variable: '--v0-font-geist-mono' })
+const _archivo = V0_Font_Archivo({ subsets: ['latin'], weight: ["100","200","300","400","500","600","700","800","900"], variable: '--v0-font-archivo' })
+const _inconsolata = V0_Font_Inconsolata({ subsets: ['latin'], weight: ["200","300","400","500","600","700","800","900"], variable: '--v0-font-inconsolata' })
 const _sourceSerif_4 = V0_Font_Source_Serif_4({ subsets: ['latin'], weight: ["200","300","400","500","600","700","800","900"], variable: '--v0-font-source-serif-4' })
-const _v0_fontVariables = `${_geist.variable} ${_geistMono.variable} ${_sourceSerif_4.variable}`
+const _v0_fontVariables = `${_archivo.variable}`
 
 export const metadata: Metadata = {
   title: "KFC Liniers",
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`font-sans ${fontVariables} ${_v0_fontVariables}`}>{children}</body>
+      <body className={`font-sans ${_v0_fontVariables}`}>{children}</body>
     </html>
   )
 }
